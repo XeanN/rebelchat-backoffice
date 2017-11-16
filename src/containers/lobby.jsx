acc493b0c8e345b49477efa833c8e74b938ae732
Javier <jegj57@gmail.com>
2017-11-16 11:52:52 -0500

    Add style for lobby and create lobby container component

---
+++ src/containers/lobby.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { connect } from "react-redux";
+import { bindActionCreators } from 'redux';
+
+import '../styles/containers/lobby.css';
+
+export class LobbyContainer extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this._notificationSystem = null;
+		this.state = {
+			email: '',
+			password: ''
+		}
+	}
+
+	componentDidMount() {
+		// this._notificationSystem = this.refs.notification;
+	}
+
+	handleChange(key, event) {
+		const state = this.state;
+		state[key] = event.target.value
+		this.setState(state);
+	}
+
+	render() {
+		return (
+			<div className="wrapper">
+				<nav id="sidebar">
+					<div id="sidebar-wrapper">
+						<div className="sidebar-header">
+							<h3>Company Name</h3>
+						</div>
+						<ul className="list-unstyled components">
+							<li><a href="#">User 1</a></li>
+							<li><a href="#">User 2</a></li>
+							<li><a href="#">User 3</a></li>
+							<li><a href="#">User 4</a></li>
+						</ul>
+					</div>
+				</nav>
+				<div id="content">
+					<nav className="navbar navbar-default">
+						<a className="navbar-brand" href="#">
+							<img src="/images/logo.png" width="30" height="30" alt=""/>
+  					</a>
+					</nav>
+					<h2>Collapsible Sidebar Using Bootstrap 3</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					<div className="line"></div>
+
+					<h2>Lorem Ipsum Dolor</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					<div className="line"></div>
+
+					<h2>Lorem Ipsum Dolor</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					<div className="line"></div>
+
+					<h3>Lorem Ipsum Dolor</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+		);
+	}
+};
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(LobbyContainer);
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NoMatch from './components/common/notfound';
 import { LoginContainer } from './containers/login';
-// import LobbyContainer from './components/app/dashboard';
+import { LobbyContainer } from './containers/lobby';
 
 const routes = (
 	<Switch>
 		<Route exact path="/" component={LoginContainer} />
 		<Route exact path="/login" component={LoginContainer} />
-		{/* <Route path="/dashboard" component={LobbyContainer} /> */}
+		<Route path="/lobby" component={LobbyContainer} />
 		<Route component={NoMatch} />
 	</Switch>
 );
