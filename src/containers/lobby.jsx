628306809357cdd6a7d82f1b15685b12513a808e
Javier <jegj57@gmail.com>
2017-11-16 12:28:05 -0500

    Add navbar styles and behavior

--- src/containers/lobby.jsx
+++ src/containers/lobby.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 import '../styles/containers/lobby.css';
 
@@ -8,27 +9,37 @@ export class LobbyContainer extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this._notificationSystem = null;
+
+		this.toggle = this.toggle.bind(this);
+		this.toogleSideBar = this.toogleSideBar.bind(this);
 		this.state = {
-			email: '',
-			password: ''
+			isOpen: false,
+			cssclass: ''
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+
+	toogleSideBar() {
+		let { cssclass } = this.state;
+		if ( cssclass ) {
+			cssclass = ''
+		} else {
+			cssclass = 'active'
 		}
-	}
-
-	componentDidMount() {
-		// this._notificationSystem = this.refs.notification;
-	}
-
-	handleChange(key, event) {
-		const state = this.state;
-		state[key] = event.target.value
-		this.setState(state);
+		this.setState({
+			cssclass
+		})
 	}
 
 	render() {
+		const { cssclass } = this.state;
 		return (
 			<div className="wrapper">
-				<nav id="sidebar">
+				<nav id="sidebar" className={cssclass}>
 					<div id="sidebar-wrapper">
 						<div className="sidebar-header">
 							<h3>Company Name</h3>
@@ -42,11 +53,22 @@ export class LobbyContainer extends React.Component {
 					</div>
 				</nav>
 				<div id="content">
-					<nav className="navbar navbar-default">
-						<a className="navbar-brand" href="#">
-							<img src="/images/logo.png" width="30" height="30" alt=""/>
-  					</a>
-					</nav>
+					<Navbar color="faded" light expand="md">
+						<NavbarBrand href="javascript:void(0)" onClick={this.toogleSideBar}>
+							<img src="/images/logo.png" width="30" height="30" alt="" />
+						</NavbarBrand>
+						<NavbarToggler onClick={this.toggle} />
+						<Collapse isOpen={this.state.isOpen} navbar>
+							<Nav className="ml-auto" navbar>
+								<NavItem>
+									<NavLink href="/components/">Components</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+								</NavItem>
+							</Nav>
+						</Collapse>
+					</Navbar>
 					<h2>Collapsible Sidebar Using Bootstrap 3</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
