3b8afb99415848ecb388a261b6d8624e1ccb555f
Javier <jegj57@gmail.com>
2017-11-24 12:18:06 -0500

    Add message template on chatzone component

--- src/components/lobby/chatzone.jsx
+++ src/components/lobby/chatzone.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Alert } from 'reactstrap';
 import { auth } from '../../lib/firebase';
 import { watchMessagesAddedEvent } from '../../actions/messages';
+import '../../styles/components/chatZone.css';
 
 export class ChatZone extends React.Component {
 
@@ -26,6 +27,24 @@ export class ChatZone extends React.Component {
 				<Alert color="light">
 					Welcome Admin, This is your space. You can see the basic backoffice's features and configure it in different ways
 				</Alert>
+					<ul className="messages">
+						<li className="message left appeared">
+							<div className="avatar"></div>
+							<div className="text_wrapper">
+								<div className="text">
+									AAAAAAAAA
+								</div>
+							</div>
+						</li>
+						<li className="message right appeared">
+							<div className="avatar"></div>
+							<div className="text_wrapper">
+								<div className="text">
+									AAAAAAAAA
+									</div>
+							</div>
+						</li>
+					</ul>
 			</div>
 		)
 	}
